83ca64f3dafb307765bf0c461bde0cd8738d75c0
Takaaki KITANO <kitanow.takaaki@gmail.com>
2025-03-10 12:57:47 +0000

    Refactor and add TODOs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { openStreamDeck, listStreamDecks } from '@elgato-stream-deck/node'
+import type { StreamDeck, StreamDeckEvents } from '@elgato-stream-deck/core'
 import jpegJS from 'jpeg-js'
 import { readFile } from 'fs/promises'
 import path from 'path'
@@ -16,18 +17,40 @@ async function main() {
     path.resolve(__dirname, '..', 'assets', 'hello.jpeg')
   )
 
+  // Suppose the device is Stream Deck Neo
+  // TODO: Add assertion of Device type
+
   primaryStreamDeck.fillLcd(
     0,
     jpegJS.decode(helloImg, { formatAsRGBA: false }).data,
     { format: 'rgb' }
   )
+
+  // TODO: Show images on keys
+  // primaryStreamDeck.fillKeyBuffer()
+
+  // Set colors of L/R touch buttons of Stream Deck Neo
   primaryStreamDeck.fillKeyColor(8, 56, 200, 56)
   primaryStreamDeck.fillKeyColor(9, 56, 56, 200)
-  primaryStreamDeck.on('up', (d) => console.log({ event: 'up', d }))
-  primaryStreamDeck.on('down', console.log)
-  primaryStreamDeck.on('lcdSwipe', console.log)
-  primaryStreamDeck.on('lcdShortPress', console.log)
-  primaryStreamDeck.on('lcdLongPress', console.log)
+
+  // Output logs for each Stream Deck Neo event for testing
+  ;(
+    [
+      'up',
+      'down',
+      'lcdSwipe',
+      'lcdShortPress',
+      'lcdLongPress',
+    ] as (keyof StreamDeckEvents)[]
+  ).forEach((eventName) => {
+    logEvent(primaryStreamDeck, eventName)
+  })
+}
+
+function logEvent(device: StreamDeck, eventName: keyof StreamDeckEvents) {
+  device.on(eventName, (...args) => {
+    console.log({ event: eventName, ...args })
+  })
 }
 
 void main()
